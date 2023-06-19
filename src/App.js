9b88dd8274d4d55574b8cb64c9f5852a9cca8419
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-19 18:19:06 +0000

    Add navlink to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Missions from './components/Missions';
+import Rocket from './components/Rocket';
+import Profile from './components/Profile';
+import PageNotFound from './components/PageNotFound';
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Project Setup</h1>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Rocket />} />
+        <Route path="missions" element={<Missions />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
