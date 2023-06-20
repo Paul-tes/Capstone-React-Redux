fdc138464ec56d54842c9faa332adf8f98377aa9
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-20 16:00:42 +0000

    Modify files

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -5,6 +5,10 @@ import './style/Rocket.css';
 
 export default function Rocket() {
   const dispatch = useDispatch();
+  // const rockets = rockets.map((rocket) => ({
+  //   ...rocket,
+  //   reserve: 'true',
+  // }));
   const { rockets, isLoading, error } = useSelector((state) => state.rockets);
 
   useEffect(() => {
@@ -15,17 +19,19 @@ export default function Rocket() {
       {isLoading && <div className="alert alert-info">Loading ........</div>}
       {error && (
         <div className="alert alert-danger">
-          Ooops! Something happend whiles fetching data
+          Ooops! Something happened whiles fetching data
         </div>
       )}
       {!isLoading
         && Object.entries(rockets).map(([id, rocket]) => (
           <div key={id} className="rocket">
-            <img src={rocket.flickr_images[0]} alt="rockets" width={300} height={220} />
+            <img src={rocket.flickr_images[0]} alt="rockets" width={250} height={200} />
             <div>
               <p className="details">{rocket.name}</p>
               <p className="description">{rocket.description}</p>
-              <button className="btn-reserve" type="button">Reserve Rocket</button>
+              <button className="btn-reserve" type="button" id={rocket.id}> Reserve Rocket</button>
+              <p>{rocket.id}</p>
+              <p>{rocket.reserve}</p>
             </div>
           </div>
         ))}
