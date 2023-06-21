1ada6763369e0a4f4e20481c2687080c84f8f9ec
paul-tes <pauasca8462@gmail.com>
2023-06-21 16:27:02 +0300

    bug introduced

--- src/components/Mission/State.jsx
+++ src/components/Mission/State.jsx
@@ -1,8 +1,11 @@
+import '../style/State.css';
+
 const State = () => (
-  <>
-    <td><button type="button">Note A Menber</button></td>
+  <div className="mission-menber">
+    <td><button type="button">Note A Menber fne</button></td>
     <td><button type="button">Join Mission</button></td>
-  </>
+    <td><button>Iam menber</button></td>
+  </div>
 );
 
 export default State;
