acfb6eae570bd8ed48108dafd8248fb18b6a8ef7
Theodore Osei Asimeng <theodoraldo@gmail.com>
2023-06-21 12:21:16 +0000

    Modify profile.js content

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { reservedRocket } from '../redux/rockets/rocketsSlice';
+import './style/Profile.css';
 
 export default function Profile() {
+  const dispatch = useDispatch();
+
+  const { rockets } = useSelector((state) => state.rockets);
+
+  useEffect(() => {
+    dispatch(reservedRocket());
+  }, [dispatch]);
   return (
-    <div>Profile</div>
+    <>
+      <div className="profile">
+        <div className="dimension">
+          <h2>My Missions</h2>
+          <p className="profile-rockets">Mission list goes here</p>
+        </div>
+        <div className="dimension">
+          <h2>My Missions</h2>
+
+          {Object.entries(rockets).map(([id, rocket]) => (
+            <div key={id} className="profile-rockets">
+              <div>
+                <p>{rocket.name}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
